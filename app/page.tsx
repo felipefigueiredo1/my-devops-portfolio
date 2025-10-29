0d1e34aeee19d784f3a1172d64ddd6f1ef0817d3
Felipe Figueiredo <felipefigueiredosal@gmail.com>
2025-10-29 19:20:11 -0300

    Comentando resumo estratégico

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
       </section>
 
       {/* Resumo Estratégico */}
-      <section className="py-16 bg-gray-50 dark:bg-gray-800">
+      {/* <section className="py-16 bg-gray-50 dark:bg-gray-800">
         <div className="container mx-auto px-6">
           <div className="max-w-4xl mx-auto">
             <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center text-gray-900 dark:text-white">
@@ -71,7 +71,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Skills Grid */}
       <section className="py-16 bg-white dark:bg-gray-900">
